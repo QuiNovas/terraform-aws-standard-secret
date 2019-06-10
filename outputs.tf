39bd2f5e618d7bb0366c7f403b43eb95ddf19659
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-10 15:18:33 +0530

    added kms key id as output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "kms_key_arn" {
   description = "The Amazon Resource Name (ARN) of the key used to encrypt the secret."
   value       = "${aws_kms_key.secret.arn}"
 }
+
+output "kms_key_id" {
+  description = "The globally unique identifier for the key used to encrypt the secret."
+  value       = "${aws_kms_key.secret.key_id}"
+}
